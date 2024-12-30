5bbf1c4c696d054b39a7eff11551e185709b4a76
rajesh <ajoypal.raj@gmail.com>
2024-12-30 12:46:57 +0600

    c12: Set Response Cookies

---
+++ 12/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+
+app.get('/one',function(req,res){
+    res.end('This is simple string response');
+})
+
+app.post('/two',function(req,res){
+    res.end('This is simple string response');
+})
+
+
+app.get('/three',function(req,res){
+    res.status(401).end('unauthorized');  
+})
+
+app.get('/four',function(req,res){
+   
+    let MyJSONArray=[
+        {
+            name: "Rajesh Pal",
+            city: "Rangamati",
+            occupation: "Engr",
+        },
+        {
+            name: "Rabbil Hasan",
+            city: "Dhaka",
+            occupation: "Engr",
+        },
+        {
+            name: "Rifat",
+            city: "Dhaka",
+            occupation: "Engr",
+        },
+        {
+            name: "Rakib",
+            city: "Rangpur",
+            occupation: "Student",
+        }
+    ]
+
+    res.json(MyJSONArray); 
+})
+
+
+app.get('/five', function(req,res){
+    res.download("./uploads/1.png");  
+})
+
+
+app.get('/bangladesh', function(req,res){
+    res.redirect("http://localhost:8000/india");
+})
+
+app.get('/india', function(req,res){
+    res.send("This is India");
+})
+
+app.get('/six', function(req,res){ 
+    res.append("name","Rajesh Pal");
+    res.append("city","Dhaka");
+    res.append("age","30 Years Old");
+    res.status(201).end("Hello world");
+})
+
+//1. Response a Cookies set korer jonno akhane notun 1ta route ami create kore nicci.
+app.get('/seven', function(req,res){ 
+    //2. cokkies set korer jonno res.cookie() k use korte hobe. akhon res.cookie() te amader first parameter hishebe key diye dite hobe and 2nd parameter a amader value diye dite hobe.
+    res.cookie("Name","Rajesh");
+    //3. Erokom onk gula cookie amra chaile set kore dite pari
+    res.cookie("City","Dhaka");
+    res.cookie("Age","24 years old");
+    //4. Erpor amader k oboshoi response k end kore dite hobe. Response end korer jonno amra res.send() ba res.end() er j kono akti k call korte hobe.
+    res.end("Successfully set cookies");
+})
+
+app.listen(8000,()=>{
+    console.log('Server is running successfully');
+})
